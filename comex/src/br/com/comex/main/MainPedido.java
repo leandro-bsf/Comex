cc2619ebceea6a87bbef054771b28d1b16e1b239
leandro-bsf <motorola1710@gmail.com>
2022-10-18 11:57:24 -0300

    corrigindo passagem de parametro

--- comex/src/br/com/comex/main/MainPedido.java
+++ comex/src/br/com/comex/main/MainPedido.java
@@ -1,14 +1,16 @@
 package br.com.comex.main;
 
 import br.com.comex.modelo.Cliente;
+import br.com.comex.modelo.Estados;
 import br.com.comex.modelo.Pedido;
+import br.com.comex.modelo.Status;
 
 public class MainPedido {
 
 	public static void main(String[] args) {
-		Cliente ana = new Cliente("Ana", "090524555" , "4691255555", "Rua antonina", "520", "Proximo a Sorveteria", "CEntro", "FC. Beltrao", "Parana");
-		Cliente eli = new Cliente("Eli", "090524556" , "4691255566", "Rua alagoas", "1550", "Proximo a mercado", "CEntro", "FC. Beltrao", "Parana");
-		Cliente gabi = new Cliente("Gabi", "090524557" , "4691255577", "Rua argentina", "1350", "Proximo a bar", "CEntro", "FC. Beltrao", "Parana");
+		Cliente ana = new Cliente("Ana", "090524555" , "4691255555", "Rua antonina", "520", "Proximo a Sorveteria", "CEntro", "FC. Beltrao", Estados.AM , Status.ATIVA);
+		Cliente eli = new Cliente("Eli", "090524556" , "4691255566", "Rua alagoas", "1550", "Proximo a mercado", "CEntro", "FC. Beltrao", Estados.PR , Status.ATIVA);
+		Cliente gabi = new Cliente("Gabi", "090524557" , "4691255577", "Rua argentina", "1350", "Proximo a bar", "CEntro", "FC. Beltrao", Estados.PR , Status.ATIVA);
         
 		Pedido P1 = new Pedido ("09/09/2022" , ana);
         Pedido P2 = new Pedido ("25/10/2022" , eli);
